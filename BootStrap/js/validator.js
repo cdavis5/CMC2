3c88a545be4bf81394d066d1ec8f2acf18eac60e
cdavis5 <cdavis@ggc.edu>
2015-09-23 13:55:24 -0400

    Made changes to the registation page

---
+++ BootStrap/js/validator.js
@@ -0,0 +1,82 @@
+function chkFields() { 
+ 
+  var init = document.getElementById("initial"); //initial password
+  var sec = document.getElementById("second"); // second password
+  var initE = document.getElementById("iemail"); //intital email
+  var secE = document.getElementById("semail"); // second email email
+ 
+    //checks if the email or password is blank
+    //check if the intital email mataches the second email
+    //check if the initial password matches the second password
+    if ((init.value == "") || (initE.value == "")){
+    alert("THERE WAS A PROBLEM WITH YOUR REQUEST! \n" +
+          "Please enter your password. \n" +
+          "Please check that your passwords match and try again. \n");   
+    return false; 
+  }
+    else
+        return true;
+}
+
+
+/*function chkEmail() { 
+  var initE = document.getElementById("iemail");
+  var secE = document.getElementById("semail");
+  if (initE.value == "") {
+    alert("You did not enter a email. \n" +
+          "Please enter one now");
+    return false;
+  }
+  if (init.value != secE.value) {
+    alert("The two email's you entered are not the same \n" +
+          "Please re-enter both now");
+    return false;
+  } else
+    return true;
+}
+*/
+
+/*function chkFields() { 
+  var init = document.getElementById("initial");
+  var sec = document.getElementById("second");
+  var initE = document.getElementById("iemail");
+  var secE = document.getElementById("semail");
+ 
+  if (init.value == "") {
+    alert("You did not enter a password \n" +
+          "Please enter one now");
+    //return false;
+  }
+    
+    if (initE.value == "") {
+    alert("You did not enter a email. \n" +
+          "Please enter one now");
+    return false;
+  }
+    
+  if (init.value != sec.value) {
+    alert("The two passwords you entered are not the same \n" +
+          "Please re-enter both now");
+    return false;
+  } else
+    return true;
+}
+*/
+/*
+  if (initE.value == "") {
+    alert("You did not enter a email. \n" +
+          "Please enter one now");
+    return false;
+  }
+  if (init.value != secE.value) {
+    alert("The two email's you entered are not the same \n" +
+          "Please re-enter both now");
+    return false;
+  } else
+    return true;
+}
+*/
+
+
+
+
---
+++ BootStrap/js/validatorr.js
@@ -0,0 +1,7 @@
+// Register the event handlers for pswd_chk.html
+
+//document.getElementById("second").onblur = chkFields; 
+document.getElementById("myForm").onsubmit = chkFields;
+
+//document.getElementById("iemail").onchange = chkEmail; 
+//document.getElementById("semail").onchange = chkEmail;
